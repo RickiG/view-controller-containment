1a5292a72b5b7e05eb02b0db6ffcbc7b4aed66cd
Ricki Gregersen <hi@input-squared.com>
2013-05-23 23:27:15 +0200

    Hooked up connections for all views and controllers with outlets

--- RGContainmentViewController/RGContainerViewController.m
+++ RGContainmentViewController/RGContainerViewController.m
@@ -8,10 +8,21 @@
 
 #import "RGContainerViewController.h"
 #import "RGMapViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
-@interface RGContainerViewController ()
+@interface RGContainerViewController () {
+    
+    CGPoint targetViewOrigin;
+    CGPoint locationViewOrigin;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *targetMapView;
+@property (weak, nonatomic) IBOutlet UIView *locationMapView;
 
 @property RGMapViewController *targetMapViewController;
+@property RGMapViewController *locationMapViewController;
+
+@property (weak, nonatomic) IBOutlet UIButton *infoButton;
 
 @end
 
@@ -21,15 +32,71 @@
 {
     [super viewDidLoad];
     
-    _targetMapViewController = [RGMapViewController new];
-    [self addChildViewController:_targetMapViewController];
-    [self.view addSubview:_targetMapViewController.view];
+    NSLog(@"%@", self.childViewControllers.description);
+    for (RGMapViewController *mapVC in self.childViewControllers) {
+        NSString *restorationID = mapVC.restorationIdentifier;
+        if ([restorationID isEqualToString:@"map_controller_target"]) {
+            
+            _targetMapViewController = mapVC;
+            
+        } else if ([restorationID isEqualToString:@"map_controller_location"]) {
+            
+            _locationMapViewController = mapVC;
+        }
+    }
+    
+    _targetMapView.layer.cornerRadius = 10.0f;
+    _locationMapView.layer.cornerRadius = 10.0f;
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    targetViewOrigin = _targetMapView.frame.origin;
+    locationViewOrigin = _locationMapView.frame.origin;
+
 }
+
+- (void) slideOut
+{
+    [UIView animateWithDuration:0.2f
+                          delay:0.0f
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         
+                         CGPoint mapOrigin = _targetMapView.frame.origin;
+                         mapOrigin.y -= CGRectGetHeight(_targetMapView.frame) - 30.0f;
+                         _targetMapView.frameOrigin = mapOrigin;
+                         
+                         mapOrigin = _locationMapView.frame.origin;
+                         mapOrigin.y += CGRectGetHeight(_locationMapView.frame) -30.0f;
+                         _locationMapView.frameOrigin = mapOrigin;
+                         
+                     } completion:nil];
+}
+
+- (void) slideIn
+{
+    [UIView animateWithDuration:0.2f
+                          delay:0.0f
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         
+                         _targetMapView.frameOrigin = targetViewOrigin;
+                         _locationMapView.frameOrigin = locationViewOrigin;
+                         
+                     } completion:nil];
+}
+
+- (IBAction)infoButtonHandler:(id)sender
+{
+    if (CGPointEqualToPoint(_targetMapView.frame.origin, targetViewOrigin) && CGPointEqualToPoint(_locationMapView.frame.origin, locationViewOrigin)) {
+        [self slideOut];
+    } else {
+        [self slideIn];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
