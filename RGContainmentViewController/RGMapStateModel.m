f327dae377156b6904846e1ab359b555fd0b3dff
Ricki Gregersen <hi@input-squared.com>
2013-05-24 10:38:53 +0200

    Added a state object to setup map appearance

---
+++ RGContainmentViewController/RGMapStateModel.m
@@ -0,0 +1,13 @@
+//
+//  RGMapStateModel.m
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/24/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import "RGMapStateModel.h"
+
+@implementation RGMapStateModel
+
+@end
