0a58fd28eddb46f259953787f430b0a513b52dd8
Ricki Gregersen <hi@input-squared.com>
2013-05-24 15:42:40 +0200

    Added annotation views for maps

---
+++ RGContainmentViewController/RGMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  RGMapAnnotation.h
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/24/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RGMapAnnotation : NSObject
+
+@end
---
+++ RGContainmentViewController/RGMapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  RGMapAnnotation.m
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/24/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import "RGMapAnnotation.h"
+
+@implementation RGMapAnnotation
+
+@end
