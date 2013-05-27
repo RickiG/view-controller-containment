2bf283d697d69715fb24cc697dec0741f2215fed
Ricki Gregersen <hi@input-squared.com>
2013-05-27 21:36:58 +0200

    Added a dedicated location manager

---
+++ RGContainmentViewController/RGLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  RGLocationController.h
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/27/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RGLocationController : NSObject
+
+@end
---
+++ RGContainmentViewController/RGLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  RGLocationController.m
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/27/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import "RGLocationController.h"
+
+@implementation RGLocationController
+
+@end
