4221fd91304a53d74f4c28fa5896af1f4ae03746
Ricki Gregersen <hi@rickigregersen.com>
2013-05-29 13:45:42 +0200

    Added annotation view to implement dragStates

---
+++ RGContainmentViewController/RGAnnotationView.h
@@ -0,0 +1,13 @@
+//
+//  RGAnnotationView.h
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/29/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface RGAnnotationView : MKAnnotationView
+
+@end
---
+++ RGContainmentViewController/RGAnnotationView.m
@@ -0,0 +1,31 @@
+//
+//  RGAnnotationView.m
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/29/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import "RGAnnotationView.h"
+
+@implementation RGAnnotationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
