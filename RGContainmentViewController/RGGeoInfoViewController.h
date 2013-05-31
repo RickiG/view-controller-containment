33f2807c8a77361a7c2f11c576ce85981cc71ca2
Ricki Gregersen <hi@rickigregersen.com>
2013-05-31 13:23:30 +0200

    Added a separate controller to display geo info

---
+++ RGContainmentViewController/RGGeoInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RGGeoInfoViewController.h
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/31/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGGeoInfoViewController : UIViewController
+
+@end
---
+++ RGContainmentViewController/RGGeoInfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  RGGeoInfoViewController.m
+//  RGContainmentViewController
+//
+//  Created by Ricki Gregersen on 5/31/13.
+//  Copyright (c) 2013 Ricki Gregersen. All rights reserved.
+//
+
+#import "RGGeoInfoViewController.h"
+
+@interface RGGeoInfoViewController ()
+
+@end
+
+@implementation RGGeoInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
